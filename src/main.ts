38b701c1e515c495d62e52bbcd0228ee99cedb8a
Dustin Do <dustin.do95@gmail.com>
2023-12-11 02:40:31 +0700

    feat: add TVPL crawler

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee'
+import { PlaywrightCrawler, Dataset } from 'crawlee'
 
-import { router } from './routes.js'
+import { router } from './routes'
 
-const startUrls = ['https://crawlee.dev']
+const startUrls = ['https://thuvienphapluat.vn']
 
 const crawler = new PlaywrightCrawler({
-  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  headless: true,
   requestHandler: router,
+  // maxConcurrency: 10,
   // Comment this option to scrape the full website.
-  maxRequestsPerCrawl: 20,
+  // maxRequestsPerCrawl: 10,
 })
 
 await crawler.run(startUrls)
--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,129 @@
+import * as path from 'path'
+import * as fs from 'fs'
+import { fileURLToPath } from 'url'
+
 import { createPlaywrightRouter } from 'crawlee'
 
 export const router = createPlaywrightRouter()
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const normalizeContent = (content: string) => {
+  // Replace line breaks surrounded by letters (word characters) with a space (removes in-sentence line breaks)
+  let newText = content.replace(/(\w)\s*\n\s*(\w)/g, '$1 $2')
+
+  // Replace multiple continuous line breaks (including spaces/tabs between line breaks) with a single line break
+  newText = newText.replace(/(\n\s*){2,}/g, '\n')
+
+  // Replace multiple continuous spaces with a single space
+  newText = newText.replace(/ +/g, ' ')
+
+  return newText
+}
+
 router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-  log.info(`enqueueing new URLs`)
+  log.info(`Starting TVPL crawler...`)
+
   await enqueueLinks({
-    globs: ['https://crawlee.dev/**'],
-    label: 'detail',
+    urls: ['https://thuvienphapluat.vn/page/tim-van-ban.aspx'],
+    label: 'list',
   })
 })
 
-router.addHandler('detail', async ({ request, page, log, pushData }) => {
-  const title = await page.title()
-  log.info(`${title}`, { url: request.loadedUrl })
+router.addHandler('list', async ({ request, enqueueLinks, log }) => {
+  log.info(`Enqueuing document list...`, { url: request.loadedUrl })
 
-  await pushData({
-    url: request.loadedUrl,
-    title,
+  await enqueueLinks({
+    // Eg: https://thuvienphapluat.vn/van-ban/Thuong-mai/Thong-bao-506-TB-VPCP-2023-ket-luan-trien-khai-Nghi-quyet-98-2023-QH15-phat-trien-Ho-Chi-Minh-589571.aspx
+    globs: ['https://thuvienphapluat.vn/van-ban/*/*.aspx'],
+    label: 'detail',
   })
+
+  log.info(`Enqueuing next pages...`)
+
+  await enqueueLinks({
+    selector: '.cmPager > a',
+    label: 'list',
+  })
+})
+
+router.addHandler('detail', async ({ request, page, log, session }) => {
+  const title = await page.title()
+
+  if (
+    title === 'THƯ VIỆN PHÁP LUẬT _ Tra cứu, Nắm bắt Pháp Luật Việt Nam' ||
+    request.loadedUrl?.includes('checkvb.aspx')
+  ) {
+    log.warning('Captcha detected. Retrying...', { url: request.url })
+
+    session?.retire()
+    await sleep(1000)
+
+    throw new Error('Captcha detected')
+  }
+
+  log.info(`${title}`)
+
+  // Get document properties from the table in tab "Thuộc tính"
+  await page.click('#ctl00_Content_ctl00_spThuocTinh')
+  await page.waitForSelector('#divThuocTinh')
+
+  const table = await page.$('#divThuocTinh > table')
+  const rows = (await table?.$$('tr')) ?? []
+  const properties: Record<string, string> = {}
+
+  for (const row of rows) {
+    const cells = await row.$$('td')
+    const textCells = (
+      await Promise.all(
+        cells.map(async (cell) => {
+          return (await cell.innerText()).trim()
+        }),
+      )
+    ).filter((text) => text !== '')
+
+    while (textCells.length > 1) {
+      const key = textCells[0].replace(/:$/, '')
+      const value = textCells[1]
+      properties[key] = value
+      textCells.splice(0, 2)
+    }
+  }
+
+  // Get document content from the tab "Nội dung"
+  await page.click('#ctl00_Content_ctl00_spNoiDung')
+  await page.waitForSelector('#divContentDoc')
+  const rawContent = await page.$eval(
+    '#divContentDoc > .content1',
+    (el) => el.textContent?.trim() ?? '',
+  )
+  const content = normalizeContent(rawContent)
+
+  // Store the document content to a file
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  const fileDir = path.join(
+    __dirname,
+    '..',
+    'documents',
+    properties['Nơi ban hành'] ?? 'unknown',
+    properties['Loại văn bản'] ?? 'unknown',
+  )
+  const fileName = `${properties['Số hiệu'].replace(/\//g, '_')}.json`
+  const filePath = path.join(fileDir, fileName)
+
+  fs.mkdirSync(fileDir, { recursive: true })
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(
+      {
+        url: request.loadedUrl,
+        title,
+        properties,
+        content,
+      },
+      null,
+      2,
+    ),
+  )
 })
